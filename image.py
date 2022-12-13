e35cfa487550ea3b98a50bb68e6cf92a04e803c8
Nguyen Quang Minh <nguyenquangminh02@vccorp.vn>
2022-12-13 16:23:40 +0700

    Mod: Sửa lỗi không đọc được ảnh trên với cv

--- image.py
+++ image.py
@@ -1,16 +1,18 @@
 import random
-import os
-from PIL import Image,ImageFilter,ImageDraw
+from PIL import Image
 import numpy as np
 import h5py
-from PIL import ImageStat
 import cv2
+
 
 def load_data(img_path,train = True):
     gt_path = img_path.replace('.jpg','.h5').replace('images','ground_truth')
     img = Image.open(img_path).convert('RGB')
     gt_file = h5py.File(gt_path)
     target = np.asarray(gt_file['density'])
+    print(target)
+    print(target.shape)
+    print(target.shape[1]/8)
     if False:
         crop_size = (img.size[0]/2,img.size[1]/2)
         if random.randint(0,9)<= -1:
@@ -27,17 +29,11 @@ def load_data(img_path,train = True):
         img = img.crop((dx,dy,crop_size[0]+dx,crop_size[1]+dy))
         target = target[dy:crop_size[1]+dy,dx:crop_size[0]+dx]
         
-        
-        
-        
+
         if random.random()>0.8:
             target = np.fliplr(target)
             img = img.transpose(Image.FLIP_LEFT_RIGHT)
     
-    
-    
-    
-    target = cv2.resize(target,(target.shape[1]/8,target.shape[0]/8),interpolation = cv2.INTER_CUBIC)*64
-    
-    
-    return img,target
+
+    target = cv2.resize(target, (int(target.shape[1]/8), int(target.shape[0]/8)), interpolation=cv2.INTER_CUBIC)*64
+    return img, target
